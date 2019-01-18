31e541dda71551ffd57f01dd42ee37d29611070b
fortu-42 <fortunatoherrerap@gmail.com>
2019-01-18 05:58:22 -0400

    Updates

---
+++ precache-manifest.55615e180c3db1572d5495fb474ab09e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "89ffd3a845c909ba3b3e",
+    "url": "/AcidLabs/static/js/runtime~main.89ffd3a8.js"
+  },
+  {
+    "revision": "bd55dde5cc4219c45a50",
+    "url": "/AcidLabs/static/js/main.bd55dde5.chunk.js"
+  },
+  {
+    "revision": "b25c37ad3400f3bd25b8",
+    "url": "/AcidLabs/static/js/1.b25c37ad.chunk.js"
+  },
+  {
+    "revision": "bd55dde5cc4219c45a50",
+    "url": "/AcidLabs/static/css/main.f08fa626.chunk.css"
+  },
+  {
+    "revision": "7ac0d767ec60dfd65e29a54a3895db2a",
+    "url": "/AcidLabs/index.html"
+  }
+];
